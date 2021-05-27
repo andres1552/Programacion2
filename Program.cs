2a96df05d501b2eed7bf2871af8caec26196b805
andres1552 <82736512+andres1552@users.noreply.github.com>
2021-05-26 22:39:25 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Programa
+{
+    internal class Programas
+    {
+        static void Main(string[] args)
+        {
+            Circulo MiCirculo;
+
+            MiCirculo = new Circulo();
+
+            Console.WriteLine(MiCirculo.CalculoArea(7));
+        }
+    }
+
+    class Circulo 
+    {
+        double pi = 3.1416;
+
+        public double CalculoArea(int radio)
+        {
+            return pi * radio * radio;
+        }
+    }
+} 
